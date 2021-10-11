033f6017dedc9389694f011fd743c9a750e18a51
Евтюшкин Алексей <44799315+ea42ru@users.noreply.github.com>
2021-10-11 16:47:43 +0300

    коррекция шаблона

--- data/param.js
+++ data/param.js
@@ -4,11 +4,11 @@ param = {
   spec: "- программист, филантроп, ...",
   about: "Описание о том какой я здоровский. И просто хороший человек!",
   soc: {
-    mail: "mail@mail.ru", 
-    github: "github",
-    telegram: "telegram",
-    skype: "skype",
-    viber: "viber"
+    mail: "<a href=\"mailto:mail@mail.ru\">mail@mail.ru</a>", 
+    github: "<a href=\"https://github.com/ea42ru\">github.com/ea42ru</a>",
+    telegram: "<a href=\"https://t.me/ea42ru\">t.me/ea42ru</a>", 
+    vk: "<a href=\"https://vk.com/id391837068\">Евтюшкин Алексей</a>", 
+    skype: "<a href=\"https://join.skype.com/invite/BdoU6AT8RwAB\">live:alex720604</a>",
+    viber: "+7(900)123-45-67" 
   }
 }
-
